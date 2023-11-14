af51bcacbdbab1d95c3351cfc29bce1485c4a3e8
Brendan Huddleston <brendanhuddleston18@gmail.com>
2023-11-14 12:46:46 -0600

    added a function to create a random name for the user drink

---
+++ js/app.js
@@ -0,0 +1,28 @@
+let nameArray = [];
+function DrinkName(name){
+  this.name = name;
+  nameArray.push(this.name);
+}
+
+let nameGenerator = function(){
+  let pickRandomName = function(){
+    return Math.floor(Math.random() * (10 - 0 ));
+  }
+  let randomNumber = pickRandomName();
+  let randomName = nameArray[randomNumber];
+  console.log(randomName);
+
+  let generatedName = [randomName];
+}
+
+new DrinkName('Midnight Mirage');
+new DrinkName('Tropical Serenity');
+new DrinkName('Velvet Sunset');
+new DrinkName('Sapphire Fizz');
+new DrinkName('Enchanted Elixir');
+new DrinkName('Citrus Bliss');
+new DrinkName('Mystic Mule');
+new DrinkName('Raspberry Breeze');
+new DrinkName('Moonlit Mojito');
+new DrinkName('Starry Sangria');
+// nameGenerator();
