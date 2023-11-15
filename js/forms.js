04eb572a2e8eb99c0aa267c16fa04408cc57e64c
Xin Deng <xind1470@gmail.com>
2023-11-14 16:58:48 -0800

    finished overall JS, hover not done

--- js/forms.js
+++ js/forms.js
@@ -1,87 +1,118 @@
-let base = document.getElementById('dropDownBase');
-let citrus = document.getElementById('dropDownCitrus');
-let bitter = document.getElementById('dropDownBitter');
-let sweet = document.getElementById('dropDownSweet');
+"use strict";
 
-let baseFunction = function(){
-  for(let i = 0;i < baseSpirits.length; i++){
-    let baseName = baseSpirits[i].name;
-    let baseOption = document.createElement('option');
-    baseOption.textContent = baseName;
-    base.appendChild(baseOption); 
-  }
-}
+let ingredientsForm = document.getElementById("ingredientsForm");
+let drinkImage = document.getElementById("recipeImage");
 
-let citrusFunction = function(){
-  for(let i = 0; i < citrusIngredients.length; i++){
-    let citrusName = citrusIngredients[i].name;
-    let citrusOption = document.createElement('option');
-    citrusOption.textContent = citrusName;
-    citrus.appendChild(citrusOption);
-  }
-}
-
-let bitterFunction = function(){
-  for(let i = 0; i < bitterIngredients.length; i++){
-    let bitterName = bitterIngredients[i].name;
-    let bitterOption = document.createElement('option');
-    bitterOption.textContent = bitterName;
-    bitter.appendChild(bitterOption);
-  }
-}
-
-let sweetFunction = function(){
-  for (i = 0; i < sweetIngredients.length; i++){
-    let sweetName = sweetIngredients[i].name;
-    let sweetOption = document.createElement('option');
-    sweetOption.textContent = sweetName;
-    sweet.appendChild(sweetOption);
-  }
-}
-baseFunction();
-citrusFunction();
-bitterFunction();
-sweetFunction();
-
-
-let ingredientsForm = document.getElementById('ingredientsForm');
-let baseSelector = document.getElementById('dropDownBase');
-let citrusSelector = document.getElementById('dropDownCitrus');
-let bitterSelector = document.getElementById('dropDownBitter');
-let sweetSelector = document.getElementById('dropDownSweet');
 let selectedIngredients = [];
 
-function handleSelection (event) {
-  let userSelection = event.target.value
-  console.log (userSelection);
+let base = document.getElementById("dropDownBase");
+let citrus = document.getElementById("dropDownCitrus");
+let bitter = document.getElementById("dropDownBitter");
+let sweet = document.getElementById("dropDownSweet");
+
+function populateDropdown(dropdown, allIngredients, ingredientDescriptions) {
+  let blankOption = document.createElement("option");
+  blankOption.textContent = "";
+  dropdown.appendChild(blankOption);
+
   for (let i = 0; i < allIngredients.length; i++) {
-    if (userSelection === allIngredients[i].name) { 
+    let ingredient = allIngredients[i];
+    let ingredientName = allIngredients[i].name;
+    let ingredientOption = document.createElement("option");
+    ingredientOption.textContent = ingredientName;
+
+    ingredientOption.title = ingredientDescriptions[i] || "";
+
+    dropdown.appendChild(ingredientOption);
+  }
+  console.log(ingredientDescriptions )
+}
+
+populateDropdown(base, baseSpirits, ingredientDescriptions);
+populateDropdown(citrus, citrusIngredients, ingredientDescriptions);
+populateDropdown(bitter, bitterIngredients, ingredientDescriptions);
+populateDropdown(sweet, sweetIngredients, ingredientDescriptions);
+
+let baseSelector = document.getElementById("dropDownBase");
+let citrusSelector = document.getElementById("dropDownCitrus");
+let bitterSelector = document.getElementById("dropDownBitter");
+let sweetSelector = document.getElementById("dropDownSweet");
+
+function handleSelection(event) {
+  let userSelection = event.target.value;
+  console.log(userSelection);
+  for (let i = 0; i < allIngredients.length; i++) {
+    if (userSelection === allIngredients[i].name) {
       let userIngredient = allIngredients[i]
       selectedIngredients.push(userIngredient);
     };
   };
-  console.log (selectedIngredients);
+  console.log(selectedIngredients);
+  removeRecipe();
 };
 
-  baseSelector.addEventListener('change', handleSelection);
-  citrusSelector.addEventListener('change', handleSelection);
-  bitterSelector.addEventListener('change', handleSelection);
-  sweetSelector.addEventListener('change', handleSelection);
+// Render Image function
+let imageArray = [];
+function renderImage() {
+  let ImageMap = function (src, alt) {
+    this.src = src;
+    this.alt = alt;
 
-
-  function renderRecipe () {
-    let recipeDisplay = document.getElementById('recipe');
-    recipeDisplay.textContent = `Here you go! Mix ${selectedIngredients[0].volume} ${selectedIngredients[0].unitOfMeasure} of ${selectedIngredients[0].name}, ${selectedIngredients[1].volume} ${selectedIngredients[1].unitOfMeasure} of ${selectedIngredients[1].name}, and ${selectedIngredients[2].volume} ${selectedIngredients[2].unitOfMeasure} of ${selectedIngredients[2].name}. Stir over ice and pour into a chilled cocktail glass. Enjoy!`;
+    imageArray.push(this);
   }
 
+  let tequilaImage = new ImageMap("img/tequila.jpg", "Tequila");
+  let rumImage = new ImageMap("img/rum.jpg", "Rum");
+  let whiskeyImage = new ImageMap("img/whiskey.jpg", "Whiskey");
+  let ginImage = new ImageMap("img/gin.jpg", "Gin");
+  console.log(imageArray);
 
-  ingredientsForm.addEventListener("submit", function(event) {
-    event.preventDefault();
-    ingredientsForm.reset();
-    dropDownBase.focus();
-    console.log (selectedIngredients);
-    renderRecipe();
-    // renderImage(); this function hasn't been made yet
+  for (let i = 0; i < imageArray.length; i++) {
+    if (selectedIngredients[0].name === imageArray[i].alt) {
+      drinkImage.src = imageArray[i].src;
+      drinkImage.alt = imageArray[i].alt;
     }
-  );
-  
+    console.log(drinkImage.src);
+  }
+}
+
+baseSelector.addEventListener("change", handleSelection);
+citrusSelector.addEventListener("change", handleSelection);
+bitterSelector.addEventListener("change", handleSelection);
+sweetSelector.addEventListener("change", handleSelection);
+
+function renderRecipe() {
+  let recipeDisplay = document.getElementById("recipe");
+  recipeDisplay.textContent = `Here you go! Mix ${selectedIngredients[0].volume} ${selectedIngredients[0].unitOfMeasure} of ${selectedIngredients[0].name}, ${selectedIngredients[1].volume} ${selectedIngredients[1].unitOfMeasure} of ${selectedIngredients[1].name}, ${selectedIngredients[2].volume} ${selectedIngredients[2].unitOfMeasure} of ${selectedIngredients[2].name}, and ${selectedIngredients[3].volume} ${selectedIngredients[3].unitOfMeasure} of ${selectedIngredients[3].name}. Stir over ice and pour into a chilled cocktail glass. Enjoy!`;
+}
+
+function removeRecipe() {
+  let recipeDisplay = document.getElementById("recipe");
+  if (recipeDisplay.textContent.trim() !== "") {
+    recipeDisplay.textContent = "";
+  };
+  if (drinkImage.src.trim() !== "") {
+    drinkImage.src = "";
+    drinkImage.alt = "";
+  };
+  if (recipeHeading.textContent.trim() !== "") {
+    recipeHeading.textContent = "";
+  };
+}
+
+ingredientsForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  ingredientsForm.reset();
+  dropDownBase.focus();
+
+  renderRecipe();
+  nameGenerator();
+  renderImage();
+  selectedIngredients = [];
+  if (imageArray.length !== 0) {
+    imageArray = [];
+    console.log(imageArray);
+  }
+  console.log(selectedIngredients);
+}
+);
--- js/ingredients.js
+++ js/ingredients.js
@@ -1,10 +1,47 @@
-baseSpirits = [];
-citrusIngredients = [];
-bitterIngredients = [];
-sweetIngredients = [];
-allIngredients = [];
+"use strict";
 
-function Ingredient(type, name, sourLevel, bitterLevel, sweetLevel, unitOfMeasure, volume) {
+let baseSpirits = [];
+let citrusIngredients = [];
+let bitterIngredients = [];
+let sweetIngredients = [];
+let allIngredients = [];
+
+
+let ingredientDescriptions = [
+    'Provides a robust and complex foundation with its rich, oaky notes, imparting warmth and depth to the cocktail',
+
+    "Infuses the drink with a crisp and botanical character, lending a refreshing and herbal complexity to the overall flavor profile",
+    
+    "Imparts a vibrant and agave-forward taste, adding a touch of sweetness and a subtle earthiness to the cocktail",
+    
+    "Brings a tropical and molasses sweetness, contributing to a smooth and rounded flavor profile with hints of caramel and spice",
+    
+    "Offers a bright and zesty acidity that balances the sweetness and enhances the overall freshness of the cocktail",
+    
+    "Adds a tangy and invigorating kick, elevating the drink with a burst of citrusy brightness",
+    
+    "Introduces a sweet and aromatic undertone, contributing a mellow citrus complexity to the cocktail",
+    
+    "Imparts a well-rounded and aromatic flavor, enhancing the depth and complexity of the cocktail with subtle herbal and spiced notes",
+    
+    "Provides a bitter and citrusy profile, balancing sweetness and adding a vibrant red hue to the drink",
+    
+    "Brings a bittersweet and orange-flavored element, offering a light and refreshing contrast to the other ingredients",
+    
+    "Introduces a unique bitterness with artichoke undertones, contributing a complex and herbal layer to the cocktail",
+    
+    "Sweetens the drink evenly, smoothing out any harsh edges and providing a well-balanced sweetness",
+    
+    "Infuses a rich and herbal sweetness, complementing the base spirit with nuanced botanical and spiced notes",
+    
+    "Adds a dry and herbaceous quality, balancing sweetness and contributing a subtle complexity to the cocktail",
+    
+    "Imparts a distinct honeyed sweetness, lending a smooth and velvety texture while introducing a natural and floral note to the overall flavor",
+  ];
+  
+
+
+function Ingredient(type, name, sourLevel, bitterLevel, sweetLevel, unitOfMeasure, volume, description) {
     this.type = type;
     this.name = name;
     this.sourLevel = sourLevel;
@@ -12,6 +49,7 @@ function Ingredient(type, name, sourLevel, bitterLevel, sweetLevel, unitOfMeasur
     this.sweetLevel = sweetLevel;
     this.unitOfMeasure = unitOfMeasure;
     this.volume = volume;
+    this.description = description;
     if (type === 'base') {
         baseSpirits.push(this); 
     };
@@ -27,21 +65,22 @@ function Ingredient(type, name, sourLevel, bitterLevel, sweetLevel, unitOfMeasur
     allIngredients.push(this);
 }
 
-const whiskey = new Ingredient ('base', 'whiskey', 0, 0, 0, 'ounces', 2.0);
-const gin = new Ingredient ('base', 'gin', 0, 0, 0, 'ounces', 2.0);
-const tequila = new Ingredient ('base', 'tequila', 0, 0, 0, 'ounces', 2.0);
-const rum = new Ingredient ('base', 'rum', 0, 0, 0, 'ounces', 2.0);
-const lemon = new Ingredient ('citrus', 'lemon', 0, 0, 0, 'ounces', 1.0);
-const lime = new Ingredient ('citrus', 'lime', 0, 0, 0, 'ounces', 1.0);
-const orange = new Ingredient ('citrus', 'orange', 0, 0, 0, 'ounces', 1.0);
-const agnostura = new Ingredient ('bitter', 'agnostura bitters', 0, 0, 0, 'dashes', 1.0);
-const campari = new Ingredient ('bitter', 'campari', 0, 0, 0, 'ounces', 1.0);
-const aperol = new Ingredient ('bitter', 'aperol', 0, 0, 0, 'ounces', 1.0);
-const cynar = new Ingredient ('bitter', 'cynar', 0, 0, 0, 'ounces', 1.0);
-const simple = new Ingredient ('sweet', 'simple syrup', 0, 0, 0, 'ounces', 1.0);
-const sweetVermouth = new Ingredient ('sweet', 'sweet vermouth', 0, 0, 0, 'ounces', 1.0);
-const dryVermouth = new Ingredient ('sweet', 'dry vermouth', 0, 0, 0, 'ounces', 1.25);
-const honey = new Ingredient ('sweet', 'honey syrup', 0, 0, 0, 'ounces', 0.75);
+const whiskey = new Ingredient('base', 'Whiskey', 0, 0, 0, 'ounces', 2.0, ingredientDescriptions[0]);
+const gin = new Ingredient('base', 'Gin', 0, 0, 0, 'ounces', 2.0, ingredientDescriptions[1]);
+const tequila = new Ingredient('base', 'Tequila', 0, 0, 0, 'ounces', 2.0, ingredientDescriptions[2]);
+const rum = new Ingredient('base', 'Rum', 0, 0, 0, 'ounces', 2.0, ingredientDescriptions[3]);
+const lemon = new Ingredient('citrus', 'Lemon', 0, 0, 0, 'ounces', 1.0, ingredientDescriptions[4]);
+const lime = new Ingredient('citrus', 'Lime', 0, 0, 0, 'ounces', 1.0, ingredientDescriptions[5]);
+const orange = new Ingredient('citrus', 'Orange', 0, 0, 0, 'ounces', 1.0, ingredientDescriptions[6]);
+const agnostura = new Ingredient('bitter', 'Agnostura Bitters', 0, 0, 0, 'dashes', 1.0, ingredientDescriptions[7]);
+const campari = new Ingredient('bitter', 'Campari', 0, 0, 0, 'ounces', 1.0, ingredientDescriptions[8]);
+const aperol = new Ingredient('bitter', 'Aperol', 0, 0, 0, 'ounces', 1.0, ingredientDescriptions[9]);
+const cynar = new Ingredient('bitter', 'Cynar', 0, 0, 0, 'ounces', 1.0, ingredientDescriptions[10]);
+const simple = new Ingredient('sweet', 'Simple Syrup', 0, 0, 0, 'ounces', 1.0, ingredientDescriptions[11]);
+const sweetVermouth = new Ingredient('sweet', 'Sweet Vermouth', 0, 0, 0, 'ounces', 1.0, ingredientDescriptions[12]);
+const dryVermouth = new Ingredient('sweet', 'Dry Vermouth', 0, 0, 0, 'ounces', 1.25, ingredientDescriptions[13]);
+const honey = new Ingredient('sweet', 'Honey Syrup', 0, 0, 0, 'ounces', 0.75, ingredientDescriptions[14]);
+
 
 console.log (baseSpirits);
 console.log (citrusIngredients);
